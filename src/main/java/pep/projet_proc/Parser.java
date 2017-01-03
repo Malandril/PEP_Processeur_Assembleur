898268de9e331171a71b54fc23e926dd64d040ed
Loic Gardaire <loic.gardaire@gmail.com>
2017-01-03 23:41:17 +0100

    Version finale

--- src/main/java/pep/projet_proc/Parser.java
+++ src/main/java/pep/projet_proc/Parser.java
@@ -2,80 +2,25 @@ package pep.projet_proc;
 
 import pep.projet_proc.condition.Condition;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * @author Loic Gardaire
- *
- * Transforme le fichier d'instructions ARM en un fichier hexadécimale
- * lisible par la ROM de l'assembleur
+ *         <p>
+ *         Transforme le fichier d'instructions ARM en un fichier hexadécimale
+ *         lisible par la ROM de l'assembleur
  */
 public class Parser {
     /**
-     * Traduit toutes les instructions du fichier en paramètre en leur valeur hexadécimale
-     * @param fileName nom du fichier à traduire
-     * @throws IOException
-     */
-    public void parseFile(String fileName) throws IOException {
-        List<String> lineList = new ArrayList<>();
-        Map<String, Integer> flagList = new HashMap<>();
-        List<List<String>> instructionList = new ArrayList<>();
-        Scanner scanner = new Scanner(new File(fileName));
-        for (int i = 0; scanner.hasNextLine(); i++) {
-            String str = scanner.nextLine();
-            str = str.replaceAll("^\\s+", "");
-            if (isFlag(str)) {
-                flagList.put(str.replace(":", ""), i);
-            }
-            lineList.add(str);
-        }
-        for (String str : lineList) {
-            if (!isFlag(str)) {
-                List<String> instruction = new ArrayList<>();
-                for (int j = 0; j < str.split(" |,").length; j++) {
-                    String tmp = str.split(" |,")[j];
-                    tmp = tmp.replaceAll("\\[", "").replaceAll("\\]", "");
-                    tmp = replaceRegisterByValue(tmp);
-                    tmp = replaceImmediateByValue(tmp);
-                    if (isBranch(tmp)) {
-                        Pattern p = Pattern.compile("([A-Z])([a-z]+)");
-                        Matcher m = p.matcher(tmp);
-                        if (m.matches()) {
-                            instruction.add(m.group(1));
-                            instruction.add(Condition.valueOf(m.group(2).toUpperCase()).getCodop());
-                        }
-                    } else if (flagList.containsKey(tmp)) {
-                        instruction.add(Integer.toBinaryString(flagList.get(tmp)));
-                    } else {
-                        instruction.add(tmp);
-                    }
-                }
-                instructionList.add(instruction);
-            }
-        }
-
-        List<String> finalList = new ArrayList<>();
-        for (int i = 0; i < instructionList.size(); i++) {
-            String[] arguments = new String[instructionList.get(i).size() - 1];
-            List<String> tmp = instructionList.get(i).subList(1, instructionList.get(i).size());
-            arguments = tmp.toArray(arguments);
-            finalList.add(InstructionFactory.getTranslation(instructionList.get(i).get(0), arguments));
-        }
-
-        BufferedWriter rom = new BufferedWriter(new FileWriter("rom.txt"));
-        rom.write("v2.0 raw");
-        for (int i = 0; i < finalList.size(); i++) {
-            rom.write("\n"+finalList.get(i) + " ");
-        }
-        rom.close();
-    }
-
-    /**
      * Ajoute les zéros nécessaires pour avoir une valeur binaire de la longueur voulue
-     * @param binaryValue valeur binaire
+     *
+     * @param binaryValue  valeur binaire
      * @param wantedLength longueur de la valeur binaire voulue
      * @return valeur binaire avec ajout des zéros
      */
@@ -87,16 +32,79 @@ public class Parser {
     }
 
     /**
+     * Traduit toutes les instructions du fichier en paramètre en leur valeur hexadécimale
+     *
+     * @param fileName nom du fichier à traduire
+     * @throws IOException
+     */
+    public void parseFile(String fileName) throws IOException {
+        List<String> lineList = new ArrayList<>();
+        Map<String, Integer> flagList = new HashMap<>();
+        List<List<String>> instructionList = new ArrayList<>();
+        Scanner scanner = new Scanner(new File(fileName));
+        int i = 0;
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine();
+            str = str.replaceAll("^\\s+", "");
+            if (isFlag(str)) {
+                flagList.put(str.replace(":", ""), i);
+            } else {
+                lineList.add(str);
+                i++;
+            }
+        }
+        for (String str : lineList) {
+            List<String> instruction = new ArrayList<>();
+            for (int j = 0; j < str.split(" |,").length; j++) {
+                String tmp = str.split(" |,")[j];
+                tmp = tmp.replaceAll("\\[", "").replaceAll("\\]", "");
+                tmp = replaceRegisterByValue(tmp);
+                tmp = replaceImmediateByValue(tmp);
+                if (isBranch(tmp)) {
+                    Pattern p = Pattern.compile("([A-Z])([a-z]+)");
+                    Matcher m = p.matcher(tmp);
+                    if (m.matches()) {
+                        instruction.add(m.group(1));
+                        instruction.add(Condition.valueOf(m.group(2).toUpperCase()).getCodop());
+                    }
+                } else if (flagList.containsKey(tmp)) {
+                    instruction.add(Integer.toBinaryString(flagList.get(tmp)));
+                } else {
+                    instruction.add(tmp);
+                }
+            }
+            instructionList.add(instruction);
+        }
+
+        List<String> finalList = new ArrayList<>();
+        for (int j = 0; j < instructionList.size(); j++) {
+            String[] arguments = new String[instructionList.get(j).size() - 1];
+            List<String> tmp = instructionList.get(j).subList(1, instructionList.get(j).size());
+            arguments = tmp.toArray(arguments);
+            finalList.add(InstructionFactory.getTranslation(instructionList.get(j).get(0), arguments));
+        }
+
+        BufferedWriter rom = new BufferedWriter(new FileWriter("rom.txt"));
+        rom.write("v2.0 raw");
+        for (int j = 0; j < finalList.size(); j++) {
+            rom.write("\n" + finalList.get(j) + " ");
+        }
+        rom.close();
+    }
+
+    /**
      * Renvoie true si la string passée en paramètre est un flag
+     *
      * @param str string à étudier
      * @return true si la string est un flag, false sinon
      */
-    public boolean isFlag(String str){
+    public boolean isFlag(String str) {
         return str.contains(":");
     }
 
     /**
      * Renvoie true si la string passée en paramètre est une instruction de branche conditionnelle
+     *
      * @param str string à étudier
      * @return true si la string est une branche, false sinon
      */
@@ -108,6 +116,7 @@ public class Parser {
 
     /**
      * Remplace un registre du banc de registres par sa valeur binaire
+     *
      * @param str string à étudier
      * @return valeur binaire du registre
      */
@@ -123,6 +132,7 @@ public class Parser {
 
     /**
      * Remplace une valeur immédiate par sa valeur binaire
+     *
      * @param str string à étudier
      * @return valeur binaire de l'immediate
      */
